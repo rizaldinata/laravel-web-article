86d199804bcec8f126e0eb6739cc0be6ba58dad2
rijuldnt <rizalldnt@gmail.com>
2024-12-05 23:41:02 +0700

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,1 @@
+alert("selamat datang di halaman home");
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home', [
+        'title' => 'Home Page'
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'title' => 'About Page',
+        'nama' => 'Rizal Risqi Ahmad Dinata'
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('blog', [
+        'title' => 'Blog Page'
+    ]);
+});
+
+Route::get('/contact', function () {
+    return view('contact', [
+        'title' => 'Contact Page'
+    ]);
+});

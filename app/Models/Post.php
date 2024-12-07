11f6bfdddfa07885b88d78f4e3171cd96790c500
rijuldnt <rizalldnt@gmail.com>
2024-12-08 01:59:51 +0700

    create model

---
+++ app/Models/Post.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Post {
+    public static function all() {
+        return [
+            [
+                'id' => 1,
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Rizal Dinata',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quod illo?
+            Commodi ipsam hic, ad atque
+            debitis earum omnis doloribus repellendus ipsum voluptatem nulla reiciendis sequi corporis blanditiis
+            obcaecati facilis.'
+            ], 
+            [
+                'id' => 2,
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Rizal Dinata',
+                'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est suscipit fugiat earum
+            laboriosam, nesciunt, laudantium quam neque cumque odio blanditiis esse deleniti voluptate tempora, sapiente
+            nemo. Aperiam officiis illum dolor.'
+            ]
+        ];
+    }
+
+    public static function find($slug) : array {
+        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug );
+
+        if(!$post) {
+            abort(404);
+        }
+
+        return $post;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -18,56 +19,13 @@ Route::get('/about', function () {
 
 Route::get('/posts', function () {
     return view('posts', [
-        'title' => 'Blog Page', 'posts' => [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Rizal Dinata',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quod illo?
-            Commodi ipsam hic, ad atque
-            debitis earum omnis doloribus repellendus ipsum voluptatem nulla reiciendis sequi corporis blanditiis
-            obcaecati facilis.'
-            ], 
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Rizal Dinata',
-                'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est suscipit fugiat earum
-            laboriosam, nesciunt, laudantium quam neque cumque odio blanditiis esse deleniti voluptate tempora, sapiente
-            nemo. Aperiam officiis illum dolor.'
-            ]
-        ]
+        'title' => 'Posts Page',
+        'posts' => Post::all()
     ]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Rizal Dinata',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quod illo?
-        Commodi ipsam hic, ad atque
-        debitis earum omnis doloribus repellendus ipsum voluptatem nulla reiciendis sequi corporis blanditiis
-        obcaecati facilis.'
-        ], 
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Rizal Dinata',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est suscipit fugiat earum
-        laboriosam, nesciunt, laudantium quam neque cumque odio blanditiis esse deleniti voluptate tempora, sapiente
-        nemo. Aperiam officiis illum dolor.'
-        ]
-    ];
-
-    $post = Arr::first($posts, function ($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
+    $post = Post::find($slug);
 
     return view('post', [
         'title' => 'single post',

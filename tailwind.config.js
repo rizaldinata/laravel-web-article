7233547ecb42ff018ffc1b57868b87ee221bda48
rijuldnt <rizalldnt@gmail.com>
2025-01-28 20:13:41 +0700

    add search

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
+    darkmode: 'class',
     content: [
       "./resources/**/*.blade.php",
       "./resources/**/*.js",
@@ -9,9 +10,35 @@ export default {
     theme: {
       extend: {
         fontFamily: {
-            sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
-          },
+          sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+          body: [
+            "inter",
+            "ui-sans-serif",
+            "system-ui",
+            "-apple-sysytem",
+            "system-ui",
+            "Segoe UI",
+            "Roboto",
+            "Helvetica Neue",
+            "Arial",
+            "Noto Sans",
+            "Sans-serif",
+            "Apple Color Emoji",
+            "Segoe UI Emoji",
+            "Segoe UI Symbol",
+            "Noto Color Emoji",
+          ]
+        },
+        colors: {
+          primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+        }
       },
     },
-    plugins: [require("flowbite/plugin")],
+    plugins: [require("flowbite/plugin"), require("flowbite-typography")],
+    safelist: [
+      "bg-red-100",
+      "bg-green-100",
+      "bg-blue-100",
+      "bg-yellow-100",
+    ]
   }
